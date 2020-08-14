76232b5d4265054e7b82f7b59e9ce17750105476
joeyskeys <joey.skeys@gmail.com>
2020-08-14 22:46:22 +0800

    add texture nodes(not finished)

--- btop/nodes/shading.py
+++ btop/nodes/shading.py
@@ -91,7 +91,7 @@ class PBRTShaderNodeDisney(PBRTShaderNode):
         'spectrans': ('NodeSocketFloat', 0),
         'speculartint': ('NodeSocketFloat', 0),
         'difftrans': ('NodeSocketVector', (1, 1, 1)),
-        'flatness': ('NodeSocketColor', (0, 0, 0)),
+        'flatness': ('NodeSocketColor', (0, 0, 0, 1)),
     }
 
     def draw_buttons(self, context, layout: 'UILayout'):
@@ -142,8 +142,8 @@ class PBRTShaderNodeGlass(PBRTShaderNodeWithRemapRoughness):
     shader_type = 'glass'
 
     socket_dict = {
-        'Kr': ('NodeSocketColor', (1.0, 1.0, 1.0)),
-        'Kt': ('NodeSocketColor', (1.0, 1.0, 1.0)),
+        'Kr': ('NodeSocketColor', (1.0, 1.0, 1.0, 1.0)),
+        'Kt': ('NodeSocketColor', (1.0, 1.0, 1.0, 1.0)),
         'eta': ('NodeSocketVector', (1.5, 1.5, 1.5)),
         'uroughness': ('NodeSocketFloat', 0),
         'vroughness': ('NodeSocketFloat', 0),
@@ -158,8 +158,8 @@ class PBRTShaderNodeHair(PBRTShaderNode):
     shader_type = 'hair'
 
     socket_dict = {
-        'sigma_a': ('NodeSocketColor', (0, 0, 0)),
-        'color': ('NodeSocketColor', (0, 0, 0)),
+        'sigma_a': ('NodeSocketColor', (0, 0, 0, 1)),
+        'color': ('NodeSocketColor', (0, 0, 0, 1)),
         'eumelanin': ('NodeSocketFloat', 1),
         'pheomelanin': ('NodeSocketFloat', 0.5),
         'eta': ('NodeSocketFloat', 1.55),
@@ -177,11 +177,11 @@ class PBRTShaderNodeKdSubsurface(PBRTShaderNodeWithRemapRoughness):
     shader_type = 'kdsubsurface'
 
     socket_dict = {
-        'Kd': ('NodeSocketColor', (0.5, 0.5, 0.5)),
+        'Kd': ('NodeSocketColor', (0.5, 0.5, 0.5, 1.0)),
         'mfp': ('NodeSocketFloat', 1),
         'eta': ('NodeSocketFloat', 1.3),
-        'Kr': ('NodeSocketColor', (1.0, 1.0, 1.0)),
-        'Kt': ('NodeSocketColor', (1.0, 1.0, 1.0)),
+        'Kr': ('NodeSocketColor', (1.0, 1.0, 1.0, 1.0)),
+        'Kt': ('NodeSocketColor', (1.0, 1.0, 1.0, 1.0)),
         'uroughness': ('NodeSocketFloat', 0),
         'vroughness': ('NodeSocketFloat', 0),
     }
@@ -195,7 +195,7 @@ class PBRTShaderNodeMatte(PBRTShaderNode):
     shader_type = 'matte'
 
     socket_dict = {
-        'Kd': ('NodeSocketColor', (0.5, 0.5, 0.5)),
+        'Kd': ('NodeSocketColor', (0.5, 0.5, 0.5, 1.0)),
         'sigma': ('NodeSocketFloat', 0),
     }
 
@@ -208,8 +208,8 @@ class PBRTShaderNodeMetal(PBRTShaderNodeWithRemapRoughness):
     shader_type = 'metal'
 
     socket_dict = {
-        'eta': ('NodeSocketColor', (1.0, 1.0, 1.0)),
-        'k': ('NodeSocketColor', (0, 0, 0)),
+        'eta': ('NodeSocketColor', (1.0, 1.0, 1.0, 1.0)),
+        'k': ('NodeSocketColor', (0, 0, 0, 1.0)),
         'roughness': ('NodeSocketFloat', 0.01),
         'uroughness': ('NodeSocketFloat', 0.01),
         'vroughness': ('NodeSocketFloat', 0.01),
@@ -250,8 +250,8 @@ class PBRTShaderNodePlastic(PBRTShaderNodeWithRemapRoughness):
     shader_type = 'plastic'
 
     socket_dict = {
-        'Kd': ('NodeSocketColor', (0.25, 0.25, 0.25)),
-        'Ks': ('NodeSocketColor', (0.25, 0.25, 0.25)),
+        'Kd': ('NodeSocketColor', (0.25, 0.25, 0.25, 1.0)),
+        'Ks': ('NodeSocketColor', (0.25, 0.25, 0.25, 1.0)),
         'roughness': ('NodeSocketFloat', 0.1),
     }
 
@@ -264,8 +264,8 @@ class PBRTShaderNodeSubstrate(PBRTShaderNodeWithRemapRoughness):
     shader_type = 'substrate'
 
     socket_dict = {
-        'Kd': ('NodeSocketColor', (0.5, 0.5, 0.5)),
-        'Ks': ('NodeSocketColor', (0.5, 0.5, 0.5)),
+        'Kd': ('NodeSocketColor', (0.5, 0.5, 0.5, 1.0)),
+        'Ks': ('NodeSocketColor', (0.5, 0.5, 0.5, 1.0)),
         'uroughness': ('NodeSocketFloat', 0.1),
         'vroughness': ('NodeSocketFloat', 0.1),
     }
@@ -282,8 +282,8 @@ class PBRTShaderNodeSubsurface(PBRTShaderNodeWithRemapRoughness):
         'sigma_a': ('NodeSocketVector', (0.0011, 0.0024, 0.014)),
         'sigma_prime_s': ('NodeSocketVector', (2.55, 3.12, 3.77)),
         'eta': ('NodeSocketFloat', 1.33),
-        'Kr': ('NodeSocketColor', (1, 1, 1)),
-        'Kt': ('NodeSocketColor', (1, 1, 1)),
+        'Kr': ('NodeSocketColor', (1, 1, 1, 1)),
+        'Kt': ('NodeSocketColor', (1, 1, 1, 1)),
         'uroughness': ('NodeSocketFloat', 0),
         'vroughness': ('NodeSocketFloat', 0),
     }
@@ -318,10 +318,10 @@ class PBRTShaderNodeTranslucent(PBRTShaderNodeWithRemapRoughness):
     shader_type = 'translucent'
 
     socket_dict = {
-        'Kd': ('NodeSocketColor', (0.25, 0.25, 0.25)),
-        'Ks': ('NodeSocketColor', (0.25, 0.25, 0.25)),
-        'reflect': ('NodeSocketColor', (0.5, 0.5, 0.5)),
-        'transmit': ('NodeSocketColor', (0.5, 0.5, 0.5)),
+        'Kd': ('NodeSocketColor', (0.25, 0.25, 0.25, 1.0)),
+        'Ks': ('NodeSocketColor', (0.25, 0.25, 0.25, 1.0)),
+        'reflect': ('NodeSocketColor', (0.5, 0.5, 0.5, 1.0)),
+        'transmit': ('NodeSocketColor', (0.5, 0.5, 0.5, 1.0)),
         'roughness': ('NodeSocketFloat', 0.1),
     }
 
@@ -334,16 +334,133 @@ class PBRTShaderNodeUber(PBRTShaderNodeWithRemapRoughness):
     shader_type = 'uber'
 
     socket_dict = {
-        'Kd': ('NodeSocketColor', (0.25, 0.25, 0.25)),
-        'Ks': ('NodeSocketColor', (0.25, 0.25, 0.25)),
-        'Kr': ('NodeSocketColor', (0, 0, 0)),
-        'Kt': ('NodeSocketColor', (0, 0, 0)),
+        'Kd': ('NodeSocketColor', (0.25, 0.25, 0.25, 1.0)),
+        'Ks': ('NodeSocketColor', (0.25, 0.25, 0.25, 1.0)),
+        'Kr': ('NodeSocketColor', (0, 0, 0, 1)),
+        'Kt': ('NodeSocketColor', (0, 0, 0, 1)),
         'roughness': ('NodeSocketFloat', 0.1),
         'urougnness': ('NodeSocketFloat', 0),
         'vroughness': ('NodeSocketFloat', 0),
         'eta': ('NodeSocketFloat', 1.5),
-        'opacity': ('NodeSocketColor', (1, 1, 1)),
+        'opacity': ('NodeSocketColor', (1, 1, 1, 1)),
     }
+
+
+# Texture nodes provides the flexibility of specifying both
+# color and float for a single socket.
+# We don't support this feature for now.
+@PBRTNodeTypes('texture')
+class PBRTShaderNodeConstant(PBRTShaderNode):
+    bl_label = 'PBRT Constant Texture'
+
+    class_type = 'PBRTShaderNodeConstant'
+    shader_type = 'constant'
+
+    socket_dict = {
+        'value': ('NodeSocketColor', (1, 1, 1, 1)),
+    }
+
+
+@PBRTNodeTypes('texture')
+class PBRTShaderNodeScale(PBRTShaderNode):
+    bl_label = 'PBRT Scale Texture'
+
+    class_type = 'PBRTShaderNodeScale'
+    shader_type = 'scale'
+
+    socket_dict = {
+        'tex1': ('NodeSocketColor', (1, 1, 1, 1)),
+        'tex2': ('NodeSocketColor', (1, 1, 1, 1)),
+    }
+
+
+@PBRTNodeTypes('texture')
+class PBRTShaderNodeMix(PBRTShaderNode):
+    bl_label = 'PBRT Mix Texture'
+
+    class_type = 'PBRTShaderNodeMix'
+    shader_type = 'mix'
+
+    socket_dict = {
+        'tex1': ('NodeSocketColor', (0, 0, 0, 1)),
+        'tex2': ('NodeSocketColor', (1, 1, 1, 1)),
+        'amount': ('NodeSocketFloat', 0.5),
+    }
+
+
+@PBRTNodeTypes('texture')
+class PBRTShaderNodeBiLerp(PBRTShaderNode):
+    bl_label = 'PBRT Bilinear Interpolation Texture'
+
+    class_type = 'PBRTShaderNodeBilerp'
+    shader_type = 'bilerp'
+
+    socket_dict = {
+        'v00': ('NodeSocketColor', (0, 0, 0, 1)),
+        'v01': ('NodeSocketColor', (1, 1, 1, 1)),
+        'v10': ('NodeSocketColor', (0, 0, 0, 1)),
+        'v11': ('NodeSocketColor', (1, 1, 1, 1)),
+    }
+
+
+@PBRTNodeTypes('texture')
+class PBRTShaderNodeImageMap(PBRTShaderNode):
+    bl_label = 'PBRT Image Map Texture'
+
+    class_type = 'PBRTShaderNodeImageMap'
+    shader_type = 'imagemap'
+
+    filename = bpy.props.StringProperty(name="filename",
+                                        description="The filename of the image to load",
+                                        default="",
+                                        subtype='FILE_PATH')
+
+    wrap = bpy.props.EnumProperty(name="wrap",
+                                  items=[
+                                      ("repeat", "Repeat", ""),
+                                      ("black", "Black", ""),
+                                      ("clamp", "Clamp", ""),
+                                  ],
+                                  default="repeat")
+
+    maxanisotropy = bpy.props.FloatProperty(name="maxanisotropy",
+                                            description="The maximum elliptical eccentricity for the EWA algorithm",
+                                            default=8,
+                                            soft_max=20,
+                                            min=0)
+
+    trilinear = bpy.props.BoolProperty(name="trilinear",
+                                       description="If true, perform trilinear interpolation when looking up pixel values",
+                                       default=False)
+
+    scale = bpy.props.FloatProperty(name="scale",
+                                    description="Scale factor to apply to value looked up in texture",
+                                    default=1,
+                                    soft_max=10,
+                                    min=0)
+
+    gamma = bpy.props.FloatProperty(name="gamma",
+                                    description="Indicates whether texel values should be converted from sRGB gamma space to linear space",
+                                    default=False)
+
+    def draw_buttons(self, context, layout: 'UILayout'):
+        layout.prop(self, 'filename')
+        layout.prop(self, 'wrap')
+        layout.prop(self, 'maxanisotropy')
+        layout.prop(self, 'trilinear')
+        layout.prop(self, 'scale')
+        layout.prop(self, 'gamma')
+        super(PBRTShaderNodeSubsurface, self).draw_buttons(context, layout)
+
+    def get_data_dict(self):
+        data_dict = super(PBRTShaderNodeSubsurface, self).get_data_dict()
+        data_dict['params']['filename'] = self.filename
+        data_dict['params']['wrap'] = self.wrap
+        data_dict['params']['maxanisotropy'] = self.maxanisotropy
+        data_dict['params']['trilinear'] = self.trilinear
+        data_dict['params']['scale'] = self.scale
+        data_dict['params']['gamma'] = self.gamma
+        return data_dict
 
 
 # Register all the shading nodes
