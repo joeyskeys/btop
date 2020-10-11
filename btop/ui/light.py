27a4fcf2b37cc9f2219b536556819b713a3fa8f0
joeyskeys <joey.skeys@gmail.com>
2020-10-11 10:59:47 +0800

    complete light UI

--- btop/ui/light.py
+++ btop/ui/light.py
@@ -19,7 +19,33 @@ import bpy
 
 
 class PBRTLightProperties(bpy.types.PropertyGroup):
-    pass
+    scale: bpy.props.FloatProperty(name="scale",
+                                         description="Scale factor that modulates the amount of light that the light source emits into the scene",
+                                         default=1,
+                                         min=0)
+
+    # Spot light props
+    coneangle: bpy.props.FloatProperty(name="coneangle",
+                                       description="The angle that the spotlight's cone makes with its primary axis",
+                                       default=30,
+                                       min=0,
+                                       max=180)
+
+    conedeltaangle: bpy.props.FloatProperty(name="conedeltaangle",
+                                            description="The angle at which the spotlight intensity begins to fall off at the edges",
+                                            default=5,
+                                            min=0,
+                                            max=90)
+
+    # Area light props
+    twosided: bpy.props.BoolProperty(name="twosided",
+                                     description="Determines whether the light source emits light from just the side of the surface where the surface normal points or both sides",
+                                     default=False)
+
+    samples: bpy.props.IntProperty(name="samples",
+                                   description="Suggested number of shadow samples to take when computing illumination from the light",
+                                   default=1,
+                                   min=1)
 
 
 class PBRT_PT_light(bpy.types.Panel):
@@ -42,5 +68,35 @@ class PBRT_PT_light(bpy.types.Panel):
 
         layout.prop(light, "type", expand=True)
         layout.prop(light, "color")
-        layout.prop(light, "")
+
         col = layout.column()
+        row = layout.row()
+
+        light_props = light.pbrt_light_props
+
+        row.prop(light_props, "scale")
+
+        if light.type == "SPOT":
+            row.prop(light_props, "coneangle")
+            row.prop(light_props, "conedeltaangle")
+        elif light.type == "AREA":
+            row.prop(light_props, "twosided")
+            row.prop(light_props, "samples")
+
+
+def register():
+    # Register property group
+    bpy.utils.register_class(PBRTLightProperties)
+    bpy.types.Light.pbrt_light_props = bpy.props.PointerProperty(type=PBRTLightProperties)
+
+    # Register UIs
+    bpy.utils.register_class(PBRT_PT_light)
+
+
+def unregister():
+    # Unregister property group
+    del bpy.types.Light.pbrt_light_props
+    bpy.utils.unregister_class(PBRTLightProperties)
+
+    # Unregister UIs
+    bpy.utils.unregister_class(PBRT_PT_light)
